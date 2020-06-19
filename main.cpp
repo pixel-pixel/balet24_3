a5d23662c3604a63eb390689fcb238a8ba548a05
Andrew Bartish <bartishco@gmail.com>
2020-06-19 16:29:50 +0300

    r

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 
+//void print(int** matrix, int size){
+//    for(int j = 0; j < size; j++){
+//        for(int i = 0; i < size; i++){
+//            std::cout << matrix[i][j] << ' ';
+//        }
+//        std::cout << std::endl;
+//    }
+//}
+
 void drawConnect(int** matrix, int size, int from){
     for(int i = 0; i < size; i++){
         if(matrix[from][i] == 1){
@@ -30,3 +39,25 @@ bool isConnected(int** matrix, int size, int from){
 
     return true;
 }
+
+//int main(){
+//    const int SIZE = 4;
+//
+//    int** matrix = new int* [SIZE];
+//
+//    for(int i = 0; i < SIZE; i++){
+//        matrix[i] = new int [SIZE];
+//        for(int j = 0; j < SIZE; j++) matrix[i][j] = 0;
+//    }
+//
+//    matrix[0][2] = 1;
+//    matrix[0][3] = 1;
+//    matrix[2][0] = 1;
+//    matrix[2][3] = 1;
+//    matrix[3][0] = 1;
+//    matrix[3][2] = 1;
+//
+//    print(matrix, SIZE);
+//
+//    std::cout << isConnected(matrix, SIZE, 0);
+//}
